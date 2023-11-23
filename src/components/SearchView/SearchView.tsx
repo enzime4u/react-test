30e5a19cf9e04bd503d163b90ca2a55d52fe1d22
Liviu Tudor <enzime4u@yahoo.com>
2023-11-23 14:22:24 +0000

    pass searchHistory as a prop to SearchView

--- src/components/SearchView/SearchView.tsx
+++ src/components/SearchView/SearchView.tsx
@@ -3,23 +3,26 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchHistory from "../SearchHistory/SearchHistory";
 
 export const SearchView = ({
-    addSearchHistory,
-    removeSearchHistory,
-    searchHistory,
+  addSearchHistory,
+  removeSearchHistory,
+  searchHistory,
 }: {
-    addSearchHistory: (postcode: string) => void;
-    removeSearchHistory: (postcode: string) => void;
-    searchHistory: string[];
+  addSearchHistory: (postcode: string) => void;
+  removeSearchHistory: (postcode: string) => void;
+  searchHistory: string[];
 }) => {
-    return (
-        <>
-            <SearchBar addSearchHistory={addSearchHistory} />
-            <div className="row align-center flex justify-around gap-20 p-20">
-                <SearchHistory
-                    removeSearchHistory={removeSearchHistory}
-                    searchHistory={searchHistory}
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <SearchBar
+        addSearchHistory={addSearchHistory}
+        searchHistory={searchHistory}
+      />
+      <div className="row align-center flex justify-around gap-20 p-20">
+        <SearchHistory
+          removeSearchHistory={removeSearchHistory}
+          searchHistory={searchHistory}
+        />
+      </div>
+    </>
+  );
 };
